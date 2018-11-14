5236a0455849594649a1f6739de1f57ccf78dc81
Juris1234 <44113168+Juris1234@users.noreply.github.com>
2018-11-14 20:58:30 +0200

    Desas
    Spele

---
+++ Desas/src/desas/Desas.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package desas;
+
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+/**
+ *
+ * @author user
+ */
+public class Desas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+        
+        SwingUtilities.invokeLater(new Runnable(){
+
+        @Override
+
+            public void run(){
+
+                JFrame frame = new JFrame();
+
+                frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+                frame.getContentPane().add(new DesasGui());
+
+                frame.pack();
+
+                frame.setVisible(true);
+
+            }
+
+        });
+    }
+    
+}
+    
